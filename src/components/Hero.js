f2769d4cb749a1d0ebbe054823004dfc661cc073
Makintomo04 <1721809@alumni.brunel.ac.uk>
2020-08-30 02:43:54 +0100

    some animations added to homepage

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,45 @@
 import React from "react"
 import heroStyles from "./hero.module.scss"
+import arrow from "../icons/arrow.png"
+import arrow2 from "../icons/arrow2.png"
+import nameSpinner from "../icons/name-spinner.png"
+import nameSpinner2 from "../icons/name-spinner2.png"
+import Typical from "react-typical"
 function Hero() {
   return (
     <div className={heroStyles.container}>
-      <h1>
+      <h1 className={heroStyles.intro}>
         Hello<span style={{ color: "red" }}>.</span>
-        <br /> I'm <br /> <span style={{ color: "red" }}>Michael</span>
+        <br /> I'm
+        <br />
+        <span style={{ color: "red" }}>Michael</span>
       </h1>
-      <p className={heroStyles.p}>I'm a frontend developer</p>
+      <div className={heroStyles.desc}>
+        <img src={arrow} height="120px" width="120px" />
+        <p className={heroStyles.p}>
+          I'm a <br />
+          <Typical
+            loop={Infinity}
+            wrapper="b"
+            steps={[
+              "frontend developer.",
+              1000,
+              "computer science graduate.",
+              1000,
+              "3D artist",
+              1000,
+              "graphic Designer.",
+              1000,
+              "food enthusiast.",
+              1000,
+            ]}
+          ></Typical>
+        </p>
+      </div>
+      <div>
+        <img src={nameSpinner2} className={heroStyles.amper} />
+        {/* <h1 className={heroStyles.amper}>&</h1> */}
+      </div>
     </div>
   )
 }
