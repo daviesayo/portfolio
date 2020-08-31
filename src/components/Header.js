b5029bae18a158434238bc33e882dd82329dfc0f
Makintomo04 <1721809@alumni.brunel.ac.uk>
2020-08-31 21:35:20 +0100

    updated logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,59 +2,63 @@ import React from "react"
 import headerStyles from "./header.module.scss"
 import { Link } from "gatsby"
 import "../styles/index.scss"
+import Logo from "../icons/pesonal_dev_logo2.png"
 function Header() {
   return (
-    <header className={headerStyles.header}>
-      <Link to="/" className={headerStyles.logo}>
-        <h2
-          className="magic-hover magic-hover__square"
-          style={{ lineHeight: "1.4rem" }}
-        >
-          MICHAEL <br />
-          AKINTOMO
-        </h2>
-      </Link>
-      <nav>
-        <ul className={headerStyles.nav}>
-          <li className={headerStyles.li}>
-            <Link
-              className={`magic-hover magic-hover__square ${headerStyles.navItem}`}
-              activeClassName={`magic-hover magic-hover__square ${headerStyles.activeNavItem}`}
-              to="/"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={`magic-hover magic-hover__square ${headerStyles.navItem}`}
-              activeClassName={`magic-hover magic-hover__square ${headerStyles.activeNavItem}`}
-              to="/about"
-            >
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={`magic-hover magic-hover__square ${headerStyles.navItem}`}
-              activeClassName={`magic-hover magic-hover__square ${headerStyles.activeNavItem}`}
-              to="/work"
-            >
-              Work
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={`magic-hover magic-hover__square ${headerStyles.navItem}`}
-              activeClassName={`magic-hover magic-hover__square ${headerStyles.activeNavItem}`}
-              to="/contact"
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <div className={headerStyles.header_wrap}>
+      <header className={headerStyles.header}>
+        <Link to="/" className={headerStyles.logo}>
+          <img src={Logo} height="40px" />
+          {/* <h2
+            className="magic-hover magic-hover__square"
+            style={{ lineHeight: "1.4rem" }}
+          >
+            MICHAEL <br />
+            AKINTOMO
+          </h2> */}
+        </Link>
+        <nav>
+          <ul className={headerStyles.nav}>
+            <li className={headerStyles.li}>
+              <Link
+                className={`magic-hover magic-hover__square ${headerStyles.navItem}`}
+                activeClassName={`magic-hover magic-hover__square ${headerStyles.activeNavItem}`}
+                to="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={`magic-hover magic-hover__square ${headerStyles.navItem}`}
+                activeClassName={`magic-hover magic-hover__square ${headerStyles.activeNavItem}`}
+                to="/about"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={`magic-hover magic-hover__square ${headerStyles.navItem}`}
+                activeClassName={`magic-hover magic-hover__square ${headerStyles.activeNavItem}`}
+                to="/work"
+              >
+                Work
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={`magic-hover magic-hover__square ${headerStyles.navItem}`}
+                activeClassName={`magic-hover magic-hover__square ${headerStyles.activeNavItem}`}
+                to="/contact"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   )
 }
 
