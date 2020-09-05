f1e61ac41b91f53b60e4aa67c9c387918b0167d4
Makintomo04 <1721809@alumni.brunel.ac.uk>
2020-09-05 14:48:12 +0100

    fixed 404 page bug by removing 'visibility : hidden' on layout.scss module + added responsiveness to 404 page.

--- src/components/NotFoundContent.js
+++ src/components/NotFoundContent.js
@@ -1,7 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
+import nfStyles from "./notFound.module.scss"
 function NotFoundContent() {
-  return <div></div>
+  return (
+    <div className={nfStyles.nfBody}>
+      <h1 className={nfStyles.heading}>
+        Oops Page Not Found<span style={{ color: "red" }}>.</span>
+      </h1>
+      <Link className={nfStyles.return} to="/">
+        Return Home
+      </Link>
+    </div>
+  )
 }
 
 export default NotFoundContent
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,26 +1,11 @@
 import React from "react"
 import Head from "../components/Head"
-import { Link } from "gatsby"
+import NotFoundContent from "../components/NotFoundContent"
 function NotFound() {
   return (
     <div>
       <Head title="404" />
-      <h1 style={{ marginTop: "20rem", marginBottom: "5rem" }}>
-        Oops Page Not Found
-      </h1>
-      <Link
-        style={{
-          textDecoration: "none",
-          fontSize: "1.8rem",
-          color: "black",
-          textTransform: "uppercase",
-          padding: "2rem",
-          border: "2px solid black",
-        }}
-        to="/"
-      >
-        Return Home
-      </Link>
+      <NotFoundContent />
     </div>
   )
 }
